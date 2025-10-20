b2081430979701afa2e9c15161faaff39ade2656
savannahjoseph10-1 <savannah.joseph10@gmail.com>
2025-10-20 09:33:49 -0400

    bunnyball

--- My project/Assets/UnityTechnologies/BunnyBall/Scripts/Key.cs
+++ My project/Assets/UnityTechnologies/BunnyBall/Scripts/Key.cs
@@ -10,12 +10,14 @@ public class Key : MonoBehaviour
     private void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
-    }
+    }//check if we collide with the player
     private void OnTriggerEnter(Collider other)
-    {
+    {// sets game over to true
         if (other.CompareTag("Player")) {
             gameManager.gameOver = true;
+            // creates a shockwave
             Instantiate(shockwavePrefab, transform.position, Quaternion.identity);
+            // destroy key
             Destroy(gameObject, 0.1f);
         }
     }
