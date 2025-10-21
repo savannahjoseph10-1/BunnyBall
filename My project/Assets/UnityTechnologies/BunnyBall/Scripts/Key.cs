85e566cdcf6f9c585a00ed7506278cf89b897d53
savannahjoseph10-1 <savannah.joseph10@gmail.com>
2025-10-21 09:28:53 -0400

    bunnyball

--- My project/Assets/UnityTechnologies/BunnyBall/Scripts/Key.cs
+++ My project/Assets/UnityTechnologies/BunnyBall/Scripts/Key.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+    // This is a variable for the shockwave Prefab
     [SerializeField] GameObject shockwavePrefab;
+    // This is a variable for the game manager 
     [SerializeField] GameManager gameManager;
 
+    // This functions runs once at the beggining of the game
     private void Start()
     {
+        // Finds the game manager 
         gameManager = FindObjectOfType<GameManager>();
-    }//check if we collide with the player
+    }
+    // This function runs when we collide with thw trigger 
     private void OnTriggerEnter(Collider other)
-    {// sets game over to true
+    {
+        // checks if we collide with something tagged as "player"
         if (other.CompareTag("Player")) {
+            // if we do collide with something tagged as "player"
+
+            //gameover is true
             gameManager.gameOver = true;
             // creates a shockwave
             Instantiate(shockwavePrefab, transform.position, Quaternion.identity);
-            // destroy key
+            // destroys this game object 
             Destroy(gameObject, 0.1f);
         }
     }
--- My project/Assets/UnityTechnologies/BunnyBall/Scripts/Player.cs
+++ My project/Assets/UnityTechnologies/BunnyBall/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     public Rigidbody rb;
     public Transform cameraTransform;
     public GameManager gameManager;
-
+    public float speed = 10f;
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -19,7 +19,7 @@ public class Player : MonoBehaviour
         forward.Normalize();
         right.Normalize();
         Vector3 direction = forward * moveVertical + right * moveHorizontal;
-        rb.AddForce(direction * 5);
+        rb.AddForce(direction * speed);
     }
 
 }
