58168ee1651bed8a3351e2dd24f6f08c35bbd512
savannahjoseph10-1 <savannah.joseph10@gmail.com>
2025-10-24 09:23:28 -0400

    bunnyball

--- My project/Assets/UnityTechnologies/BunnyBall/Scripts/Player.cs
+++ My project/Assets/UnityTechnologies/BunnyBall/Scripts/Player.cs
@@ -8,13 +8,17 @@ public class Player : MonoBehaviour
     public Transform cameraTransform;
     public GameManager gameManager;
     public float speed = 10f;
+    public int jumpforce = 100;
     private int x = 0;
+
     void Update()
     {
         x = x + 1;
-        Debug.Log(message: "Hello" + x); 
+        //Debug.Log(message: "Hello" + x);
+        
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+
         Vector3 forward = cameraTransform.forward;
         Vector3 right = cameraTransform.right;
         forward.y = 0;
@@ -23,6 +27,11 @@ public class Player : MonoBehaviour
         right.Normalize();
         Vector3 direction = forward * moveVertical + right * moveHorizontal;
         rb.AddForce(direction * speed);
-    }
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space was pressed");
+            rb.AddForce(Vector3.up * jumpforce);
+        }
+    }
 }
