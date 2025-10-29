2634ff7353eca6f6ac2685e44f610ac0e628eb97
savannahjoseph10-1 <savannah.joseph10@gmail.com>
2025-10-29 09:34:16 -0400

    bunnyball

---
+++ My project/Assets/UnityTechnologies/BunnyBall/Scripts/FallingPlatform.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb.useGravity = false;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            rb.useGravity = true;
+            Debug.Log("Collided with Player");
+        }
+    }
+
+}
--- My project/Assets/UnityTechnologies/BunnyBall/Scripts/Player.cs
+++ My project/Assets/UnityTechnologies/BunnyBall/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     public float speed = 10f;
     public int jumpforce = 100;
     private int x = 0;
+    private bool isGrounded = false;
 
     void Update()
     {
@@ -28,10 +29,27 @@ public class Player : MonoBehaviour
         Vector3 direction = forward * moveVertical + right * moveHorizontal;
         rb.AddForce(direction * speed);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&& isGrounded == true)
         {
             Debug.Log("Space was pressed");
             rb.AddForce(Vector3.up * jumpforce);
         }
     }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
 }
+
